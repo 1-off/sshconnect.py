8358f85265498b72254ecff1e328867467063672
1-off <97915378+1-off@users.noreply.github.com>
2022-03-02 17:25:22 +0100

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,15 +1,21 @@
 import paramiko
 
-class ssh:
-    def __init__(self,host,port,username,password,command):
+class ssh_y:
+    def __init__(self,host,port,username,password):
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.host = host
         self.port = port
         self.username= username
         self.password = password
-        self.command = command
         self.ssh.connect(self.host, self.port, self.username, self.password)
 
     def execute(self,command:str):
-        self.ssh.exec_command(command)
+        output = ''
+        stdin, stdout, sterr = self.ssh.exec_command(command)
+        for x in stdout:
+            output+=line.strip('\n')
+        print(output)
+        
+ 
+        
